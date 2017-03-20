adead2dad4dee63aaa5c05f40025afc3f3e556de
saihemanthKGP <saihemanth.kng@gmail.com>
2017-03-20 09:12:16 +0530

    my first algo

---
+++ insertion_sort.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+void insertion_sort(int *a,int n){
+	int i,j,key;
+	for(i=1;i<n;i++){
+	   key=a[i];
+	   for(j=i-1;j>=0&&a[j]>key;j--){
+	      a[j+1]=a[j];
+	   }
+	   a[j+1]=key;
+	}
+return;	
+}
+
+int main(){
+	int i,n;
+	scanf("%d",&n);
+	int *a=(int *)malloc(sizeof(int)*n);
+	for(i=0;i<n;i++)scanf("%d",&a[i]);
+	insertion_sort(a,n);
+	for(i=0;i<n;i++)printf("%d ",a[i]);	
+return 0;	
+}
