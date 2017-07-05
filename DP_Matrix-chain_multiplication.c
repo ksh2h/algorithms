e89f29810b865e27d624b443485524b199e5f5fd
saihemanthKGP <saihemanth.kng@gmail.com>
2017-07-05 22:27:24 +0530

    Memoization method added

--- DP_Matrix-chain_multiplication.c
+++ DP_Matrix-chain_multiplication.c
@@ -3,9 +3,54 @@
 #include <math.h>
 #include <stdlib.h>
 #include <limits.h>
-//space complexity = O(n^2).
+
+/*Very inefficient algorithm
+ *Simply follows the recursive property.
+ *Atleast exponential running time.
+ */
+int recursivematrixchain(int *p,int ** m,int i,int j){
+    if(i==j)return 0;
+    int k,q;
+    m[i][j]=INT_MAX;
+    for(k=i;k<j;k++){
+        q=recursivematrixchain(p,m,i,k)+recursivematrixchain(p,m,k+1,j)+p[i]*p[k+1]*p[j+1];
+        if(q<m[i][j])m[i][j]=q;
+    }
+    return m[i][j];
+}
 
 
+
+/*Memoization method.
+ *Worst case running time = O(n^3).
+ */
+int lookuptable(int *p,int **m,int i,int j){
+    if(m[i][j]==INT_MAX){
+        int k,q;
+        if(i==j)m[i][j]=0;
+        for(k=i;k<j;k++){
+            q=lookuptable(p,m,i,k)+lookuptable(p,m,k+1,j)+p[i]*p[k+1]*p[j+1];
+            if(q<m[i][j])m[i][j]=q;
+        }
+    }
+    return m[i][j];
+}
+
+int memoizedmatrixchain(int *p,int n){
+    int **m=(int **)malloc(sizeof(int *)*n);
+    int i,j;
+    for(i=0;i<n;i++){
+        m[i]=(int *)malloc(sizeof(int)*n);
+        for(j=i;j<n;j++)m[i][j]=INT_MAX;
+    }
+    return lookuptable(p,m,0,n-1);
+}
+
+
+
+
+/*Tabulation method.
+ */
 void matrixchainorder(int *p,int **m,int **s,int n){//worst case time complexity = O(n^3).
     int i,j,k,l,q;
     for(i=0;i<n;i++)m[i][i]=0;
@@ -52,7 +97,7 @@ int main() {
     scanf("%d",&p[n]);
     matrixchainorder(p,m,s,n);
     print_optimal_parens(s,0,n-1);
-    printf("\nOptimal cost of multiplication : %d\n",m[0][n-1]);
+    printf("\nOptimal cost of multiplication (Tabulation): %d\nOptimal cost of multiplication (Memoization): %d\n",m[0][n-1],memoizedmatrixchain(p,n));
     return 0;
 }
 
