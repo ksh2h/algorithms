2eae0fd770a243d962e84339cd32f47d762f8859
saihemanthKGP <saihemanth.kng@gmail.com>
2017-07-07 21:26:07 +0530

    Space reduced by constant factor

--- DP_LCS.c
+++ DP_LCS.c
@@ -12,20 +12,16 @@
  *Memoization method.
  *Worst case time complexity = O(m*n).
  */
-int lcs(char *x,char *y,int **b,int **c,int i,int j){
+int lcs(char *x,char *y,int **c,int i,int j){
     if(c[i][j]==-1){
         if(x[i-1]==y[j-1]){
-            //printf("i:%d,j:%d,z[%d]=1\n",i,j,i);
-            b[i][j]=1;
-            c[i][j]=1+lcs(x,y,b,c,i-1,j-1);
+            c[i][j]=1+lcs(x,y,c,i-1,j-1);
         }
-        else if(lcs(x,y,b,c,i-1,j)>=lcs(x,y,b,c,i,j-1)){
+        else if(lcs(x,y,c,i-1,j)>=lcs(x,y,c,i,j-1)){
             c[i][j]=c[i-1][j];
-            b[i][j]=2;
         }
         else {
             c[i][j]=c[i][j-1];
-            b[i][j]=3;
         }
     }
     return c[i][j];
@@ -33,14 +29,14 @@ int lcs(char *x,char *y,int **b,int **c,int i,int j){
 
 /*Worst case time complexity = O(m+n).
  */
-void printans(char *x,int **b,int i,int j){
+void printans(char *x,int **c,int i,int j){
     if(i<1||j<1)return;
-    if(b[i][j]==1){
-        printans(x,b,i-1,j-1);
+    if(c[i][j]==1+c[i-1][j-1]){
+        printans(x,c,i-1,j-1);
         printf("%c",x[i-1]);
     }
-    else if(b[i][j]==2)printans(x,b,i-1,j);
-    else printans(x,b,i,j-1);
+    else if(c[i][j]==c[i-1][j])printans(x,c,i-1,j);
+    else printans(x,c,i,j-1);
     return;
 }
 
@@ -79,19 +75,17 @@ int main() {
     scanf("%s%s",x,y);
     m=strlen(x);
     n=strlen(y);
-    int **b=(int **)malloc((m+1)*sizeof(int));
     int **c=(int **)malloc(sizeof(int *)*(m+1));
     for(i=0;i<=m;i++){
         c[i]=(int *)malloc(sizeof(int)*(n+1));
-        b[i]=(int *)malloc(sizeof(int)*(n+1));
         for(j=0;j<=n;j++){
             if(!i||!j)c[i][j]=0;
             else c[i][j]=-1;
         }
     }
-    printf("The length of longest common subsequence is :\nMemoization : %d\nTabulation : %d\n",lcs(x,y,b,c,m,n),lcs_t(x,y,m,n));
+    printf("The length of longest common subsequence is :\nMemoization : %d\nTabulation : %d\n",lcs(x,y,c,m,n),lcs_t(x,y,m,n));
     printf("The longest common subsequence of X,Y is : ");
-    printans(x,b,m,n);
+    printans(x,c,m,n);
     printf("\n");
     return 0;
 }
